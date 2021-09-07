c9dea6105c9aa4d61a21115213f636310508cbee
darshan magare <darshan.magare1234@gmail.com>
2021-09-07 09:43:00 +0530

    Add project files.

---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+public interface WildAnimal
+{
+    void taste();   
+}
+
+public interface PetAnimal
+{
+    void Color();
+}
+
+public class InterfaceImplementation : WildAnimal,PetAnimal
+{
+    public void taste()
+    {
+        Console.WriteLine("Sensing different tastes");
+
+    }
+    public void Color()
+    {
+        Console.WriteLine("Colorful");
+
+    }
+
+
+
+    abstract class interfaceAbstractimp :PetAnimal,WildAnimal
+    {
+        public void Color()
+        {
+            Console.WriteLine("Colorful");
+
+        }
+
+    }
+}
